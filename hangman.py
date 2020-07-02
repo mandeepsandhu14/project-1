d7deea1feb2cc0cbf706a0767a3a44ed5bbbc358
Mandeep Sandhu <mandeepsan14@gmail.com>
2020-07-02 13:04:51 -0700

    hangman code py

---
+++ hangman.py
@@ -0,0 +1,52 @@
+hangman.py
+
+import random
+
+Name = input("Your first name")
+
+print ("Good Luck!", name)
+
+words = ['rainbow, science, programming, python, board, water, chicken']
+
+word = random.choice(words)
+
+print("guess the characters")
+
+guesses = ''
+
+turns = 10
+
+while turns > 0:
+
+failed = 0
+
+for char in word:
+
+  if char in guesses:
+    print(char)
+
+    else:
+      print("_")
+
+      failed += 1
+
+if failed == 0;
+
+print("You Win")
+
+print("The word is:", word)
+break
+
+guess = input("guess a character:")
+
+guesses += guess
+
+if guess not in word:
+  
+  Print("Wrong")
+
+  print("you have", + turn, 'more guesses')
+
+  if turns == 0;
+    
+    print("you loose")
